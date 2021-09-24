ee75b28a88735581892d8746edcc05a3ac55c844
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-24 14:40:21 +0200

    Empty functions for queries.

--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -1,6 +1,28 @@
+using System;
+using System.Collections.Generic;
+
 namespace BDSA2020.Assignment03
 {
     public class Queries
     {
+        public static IEnumerable<Wizard> WizardsByAuthor(string author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int FirstAppearanceOfSith()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static (string, int) WizardsFromProperty(string property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<Dictionary<string, IEnumerable<Wizard>>> WizardsByCreators()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
