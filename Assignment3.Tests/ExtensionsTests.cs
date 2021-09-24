3e17d605c73892183784a353605a2cb167ffd4a6
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-24 12:39:31 +0200

    Flattening test

--- Assignment3.Tests/ExtensionsTests.cs
+++ Assignment3.Tests/ExtensionsTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BDSA2020.Assignment02.Tests
@@ -6,9 +7,14 @@ namespace BDSA2020.Assignment02.Tests
     public class ExtensionsTests
     {
         [Fact]
-        public void Test1()
+        public void Flattening_Works_On_Sample()
         {
-
+            // Arrange
+            IEnumerable<int>[] xs = { new int[]{ 1, 2, 3 }, new int[]{ 4, 5 }, new int[]{ 6, 7, 8, 9 } };
+            // Act
+            var output = xs.Flatten();
+            // Assert
+            Assert.Equal(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, output);
         }
     }
 }
