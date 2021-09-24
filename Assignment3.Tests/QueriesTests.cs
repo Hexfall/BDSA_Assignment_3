6499b13aa7d64750ea48922d6b77aa8f78672cb5
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-24 14:54:55 +0200

    Tests for HP Wizards

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
+using static BDSA2020.Assignment03.Queries;
 
 namespace BDSA2020.Assignment03.Tests
 {
     public class QueriesTests
     {
         [Fact]
-        public void Test1()
+        public void Finds_Wizards_By_Rowling()
         {
-
+            // Arrange & Act
+            var wiz = WizardsByAuthor("J.K.Rowling");
+            
+            // Assert
+            Assert.Equal(new HashSet<string> { "Harry Potter", "Hermione Granger", "Ron Weasley", "Severus Snape", "Dumbledore" }, new HashSet<string> (wiz));
         }
     }
 }
