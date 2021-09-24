e942400698628ccb733db472a71aa1a1f0ab4ece
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-24 13:13:09 +0200

    Implemented IsSecure

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -8,7 +8,7 @@ namespace BDSA2020.Assignment03
     {
         public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items) => items.Aggregate((IEnumerable<T>)new List<T>(), (agg, item) => agg.Concat(item));
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate) => items.Where(o => predicate(o));
-        public static bool IsSecure(this Uri uri) => throw new NotImplementedException();
+        public static bool IsSecure(this Uri uri) => uri.AbsoluteUri.ToLower().StartsWith("https");
         public static int WordCount(this string str) => throw new NotImplementedException();
     }
 }
