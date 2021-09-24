6dc38c45196e397db74a8c7abc63abe8ff8d7b25
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-24 12:55:43 +0200

    Implemented Flatten.

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BDSA2020.Assignment03
 {
     public static class Extensions
     {
-        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items) => items.Aggregate((IEnumerable<T>)new List<T>(), (agg, item) => agg.Concat(item));
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
         public static bool IsSecure(this Uri uri) => throw new NotImplementedException();
         public static int WordCount(this string str) => throw new NotImplementedException();
