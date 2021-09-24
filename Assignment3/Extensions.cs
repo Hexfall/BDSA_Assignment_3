d7a575695016f12472588d99357a048ef8505a19
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-24 12:33:05 +0200

    Empty extension functions.

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -1,8 +1,13 @@
 using System;
+using System.Collections.Generic;
 
 namespace BDSA2020.Assignment02
 {
     public static class Extensions
     {
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+        public static bool IsSecure(this Uri uri) => throw new NotImplementedException();
+        public static int WordCount(this string str) => throw new NotImplementedException();
     }
 }
