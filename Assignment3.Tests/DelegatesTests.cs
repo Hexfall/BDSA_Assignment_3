06df1f7dec8244ec9a0d4df1906511c2c4412d27
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-24 14:10:14 +0200

    Decimal tests.

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 namespace BDSA2020.Assignment03.Tests
 {
     public delegate string stringConverter(string s);
+    public delegate decimal decimalManipulator(decimal d1, decimal d2);
     public class DelegatesTests
     {
         [Theory]
@@ -13,13 +14,26 @@ namespace BDSA2020.Assignment03.Tests
         [InlineData("ab", "ba")]
         [InlineData("abcdefghijklmnopqrstuvwxyz", "zyxwvutsrqponmlkjihgfedcba")]
         [InlineData("Jeg heder viktor og jeg programmerer, fordi jeg er en stor dreng!", "!gnerd rots ne re gej idrof ,reremmargorp gej go rotkiv redeh geJ")]
-        public void Test1(string org, string expected)
+        public void String_Reversal_Works_On_Sample_Strings(string org, string expected)
         {
             // Arrange & Act
             var output = new stringConverter((string s) => s.ToCharArray().Aggregate("", (agg, cur) => cur + agg))(org);
 
             // Assert
             Assert.Equal(expected, output);
+        }
+
+        [Theory]
+        [InlineData(1.1d, 1.0d, 1.1d)]
+        [InlineData(1.1d, 1.1d, 1.21d)]
+        [InlineData(2d, 1.1d, 2.2d)]
+        public void Decimal_Product_Returns_Correctly_For_Sample_Decimals(decimal d1, decimal d2, decimal expectedOutcome)
+        {
+            // Arrange & Act
+            var output = new decimalManipulator((d1, d2) => throw new NotImplementedException())(d1, d2);
+
+            // Assert
+            Assert.Equal(expectedOutcome, output);
         }
     }
 }
