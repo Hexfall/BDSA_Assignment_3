a09cb606e3313771128624288224cd62bbd07add
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-24 13:53:37 +0200

    Tests for string reversal.

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -3,12 +3,25 @@ using Xunit;
 
 namespace BDSA2020.Assignment03.Tests
 {
+    public delegate string stringConverter(string s);
     public class DelegatesTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("ab", "ba")]
+        [InlineData("abcdefghijklmnopqrstuvwxyz", "zyxwvutsrqponmlkjihgfedcba")]
+        [InlineData("Jeg heder viktor og jeg programmerer, fordi jeg er en stor dreng!", "!gnerd rots ne re gej idrof ,reremmargorp gej go rotkiv redeh geJ")]
+        public void Test1(string org, string expected)
         {
+            // Arrange
+            stringConverter reverser = new stringConverter((string s) => throw new NotImplementedException());
 
+            // Act
+            var output = reverser(org);
+
+            // Assert
+            Assert.Equal(expected, output);
         }
     }
 }
